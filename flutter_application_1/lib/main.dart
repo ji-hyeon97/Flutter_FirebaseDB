54ec14e49b7b6dd489f8df5fbf6eab3ac6436dd4
ji-hyeon97 <79920930+ji-hyeon97@users.noreply.github.com>
2021-09-02 23:00:19 +0900

    Update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'information.dart';
-void main() => runApp(MyApp());
+import 'package:firebase_core/firebase_core.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
